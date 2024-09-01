823008d19296416319f4f64af165ea0a187e3702
erict-tinaza <tinaza.jhannerict.catabay@gmail.com>
2024-09-01 22:34:41 +0800

    Add lcd menu, helper functions, and ability to interact with lcd menu

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 
-LiquidCrystal_I2C lcd_1(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+Servo servo1;
+Servo servo2;
 
-//Control button for the menu
+// Control button for the menu
 const int upButton = 2;
 const int downButton = 3;
 const int selectButton = 4;
@@ -12,12 +14,146 @@ int menu = 1;
 int timer_1 = 0;
 int timer_2 = 0;
 
-//Servo Pins
+// Servo Pins
 const int servoPin1 = 5;
 const int servoPin2 = 6;
 
-//Inductive and Capacitive Sensor Pins
-const int capacitiveSensor = 7;
-const int inductiveSensor = 8;
+// Inductive and Capacitive Sensor Pins
+const int capacitiveSensorPin = 7;
+const int inductiveSensorPin = 8;
 
 
+// Function definitions here
+int readCapacitiveSensorData(){
+  int sensorVal = digitalRead(capacitiveSensorPin);
+  //delay(1000);
+  return sensorVal;
+}
+
+int readInductiveSensorData(){
+  int sensorVal = digitalRead(inductiveSensorPin);
+  return sensorVal;
+}
+
+void rotateServo(Servo &servo, int angle){
+  servo.write(angle);
+}
+
+void openCloseBinLid(int lidNum, bool toOpen){
+  int angle = toOpen ? 180 : 0;
+  if (lidNum == 1) {
+    rotateServo(servo1, angle);
+  } else if (lidNum == 2) {
+    rotateServo(servo2, angle);
+  }
+}
+
+void waitForObjectPresence(){
+  unsigned long startTime = millis();  
+  
+  while (readCapacitiveSensorData() == 0) {
+    if (millis() - startTime >= 3000) {
+      lcd.clear();
+      lcd.print("No bottle inside!");
+      lcd.print("Closing bin...");
+      openCloseBinLid(1, false);
+      delay(3000);
+      break;
+    }
+    
+  }
+  lcd.clear();
+  lcd.print("Object present");
+  
+  
+}
+
+void updateMenu(){
+  switch (menu) {
+    case 0:
+      menu = 1;
+      break;
+    case 1:
+      lcd.clear();
+      lcd.print(">Deposit");
+      lcd.setCursor(0,1);
+      lcd.print(" Redeem");
+      break;
+    case 2:
+      lcd.clear();
+      lcd.print(" Deposit");
+      lcd.setCursor(0,1);
+      lcd.print(">Redeem");
+      break;
+    case 3:
+      menu = 2;
+      break;
+  }
+}
+
+void deposit(){
+  lcd.clear();
+  lcd.print("Opening bin....");
+  openCloseBinLid(1, true);
+  lcd.clear();
+  lcd.print("Insert Bottle!");
+  delay(3000);
+  waitForObjectPresence();
+  openCloseBinLid(1, false);
+}
+
+void redeem(){
+
+}
+
+void executeAction() {
+  switch (menu) {
+    case 1:
+      deposit();
+      break;
+    case 2:
+      redeem();
+      break;
+  }
+}
+
+void setup(){
+  lcd.init();
+  lcd.backlight();
+  pinMode(upButton, INPUT_PULLUP);  
+  pinMode(downButton, INPUT_PULLUP);  
+  pinMode(selectButton, INPUT_PULLUP);  
+  pinMode(capacitiveSensorPin, INPUT);
+  pinMode(inductiveSensorPin, INPUT);
+  
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  
+  updateMenu();
+  Serial.begin(9600);  
+}
+
+void loop(){
+  // Display sensor reading to serial monitor
+  Serial.print("Sensor value: ");
+ Serial.println(readCapacitiveSensorData());
+ // Serial.println(readInductiveSensorData());
+  if (!digitalRead(downButton)){
+    menu++;
+    updateMenu();
+    delay(100);
+    while (!digitalRead(downButton));
+  }
+  if (!digitalRead(upButton)){
+    menu--;
+    updateMenu();
+    delay(100);
+    while(!digitalRead(upButton));
+  }
+  if (!digitalRead(selectButton)){
+    executeAction();
+    updateMenu();
+    delay(100);
+    while (!digitalRead(selectButton));
+  }                        
+}
