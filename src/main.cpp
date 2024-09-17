46b9f74aebeda0492aabf62f2d404b54515cb5fe
erict-tinaza <tinaza.jhannerict.catabay@gmail.com>
2024-09-17 21:39:17 +0800

    Refractor the deposite feature to make it better by adding submenu's and related actions

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servo1;
 Servo servo2;
 
-/*TODO: SIM800L can needs atleast 6.2v from the dc to dc converter or 
-*Fix the issue by replacing the capacitor of the module by a 16V 1000 microfarads capacitor to provide enough power to the module
-*during high peak usage of the said module.
-*/
+/*TODO: SIM800L can needs atleast 6.2v from the dc to dc converter or
+ *Fix the issue by replacing the capacitor of the module by a 16V 1000 microfarads capacitor to provide enough power to the module
+ *during high peak usage of the said module.
+ */
 // Pin definitions
 const int upButton = 2;
 const int downButton = 3;
@@ -21,32 +21,34 @@ const int inductiveSensorPin = 8;
 const int PIN_RED = 9;
 const int PIN_GREEN = 10;
 const int PIN_BLUE = 11;
-const int POINTS_BLOCK = 4; //Block were the poinst will be stored in the RFID memory
-//RFID PIN Definitions
-#define SS_PIN 53 //Green
-#define RST_PIN 49 //Grey
+const int POINTS_BLOCK = 4; // Block were the poinst will be stored in the RFID memory
+// RFID PIN Definitions
+#define SS_PIN 53  // Green
+#define RST_PIN 49 // Grey
 MFRC522::StatusCode authenticateBlock(int blockNumber);
-MFRC522 mfrc522 (SS_PIN, RST_PIN);
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
 
-
-
 // Menu structure
-struct MenuItem {
-    const char* name;
+struct MenuItem
+{
+    const char *name;
     void (*action)();
 };
 
-struct Menu {
-    MenuItem* items;
+struct Menu
+{
+    MenuItem *items;
     int itemCount;
     int currentItem;
-    const char* title;
+    const char *title;
 };
 
 // Function prototypes
 void depositAction();
 void redeemAction();
+void storePointsAction();
+void insertAnotherBottleAction();
 void redeemPointsAction();
 void setUpRFID();
 bool detectCard();
@@ -56,32 +58,38 @@ void decrementPoints(int amount);
 void displayPoints(int points);
 int readPoints();
 
-
 // Global variables
 bool isObjectInside = false;
-int totalPoints = 0; 
+int totalPoints = 0;
 int pointsToRedeem = 0;
 
 // Define menu items
 MenuItem mainMenuItems[] = {
     {"Deposit", depositAction},
-    {"Redeem", redeemAction}
-};
+    {"Redeem", redeemAction}};
+
+MenuItem postDepositMenuItems[] = {
+    {"Insert Another", insertAnotherBottleAction},
+    {"Redeem Points", redeemPointsAction},
+    {"Store on RFID", storePointsAction}};
 
 // Define menus
 Menu mainMenu = {mainMenuItems, 2, 0, "Main Menu"};
-
-Menu* currentMenu = &mainMenu;
+Menu postDepositMenu = {postDepositMenuItems, 3, 0, "Options"};
+Menu *currentMenu = &mainMenu;
 
 // Function definitions
-void pulseColor(int redValue, int greenValue, int blueValue) {
-    for (int brightness = 0; brightness <= 255; brightness += 5) {
+void pulseColor(int redValue, int greenValue, int blueValue)
+{
+    for (int brightness = 0; brightness <= 255; brightness += 5)
+    {
         analogWrite(PIN_RED, (redValue * brightness) / 255);
         analogWrite(PIN_GREEN, (greenValue * brightness) / 255);
         analogWrite(PIN_BLUE, (blueValue * brightness) / 255);
         delay(10);
     }
-    for (int brightness = 255; brightness >= 0; brightness -= 5) {
+    for (int brightness = 255; brightness >= 0; brightness -= 5)
+    {
         analogWrite(PIN_RED, (redValue * brightness) / 255);
         analogWrite(PIN_GREEN, (greenValue * brightness) / 255);
         analogWrite(PIN_BLUE, (blueValue * brightness) / 255);
@@ -89,65 +97,77 @@ void pulseColor(int redValue, int greenValue, int blueValue) {
     }
 }
 
-void ledStatusCode(int errorCode) {
-    switch (errorCode) {
-        case 200: // Green Indicator = Ok
-            analogWrite(PIN_RED, 0);
-            analogWrite(PIN_GREEN, 255);
-            analogWrite(PIN_BLUE, 0);
-            break;
-        case 201:
-            pulseColor(255, 0, 0);
-            break;
-        case 102: // Orange indicator = Processing
-            pulseColor(255, 60, 5);
-            break;
-        case 404: // Red indicator = error
-            pulseColor(255, 0, 0);
-            break;
+void ledStatusCode(int errorCode)
+{
+    switch (errorCode)
+    {
+    case 200: // Green Indicator = Ok
+        analogWrite(PIN_RED, 0);
+        analogWrite(PIN_GREEN, 255);
+        analogWrite(PIN_BLUE, 0);
+        break;
+    case 201:
+        pulseColor(255, 0, 0);
+        break;
+    case 102: // Orange indicator = Processing
+        pulseColor(255, 60, 5);
+        break;
+    case 404: // Red indicator = error
+        pulseColor(255, 0, 0);
+        break;
     }
 }
 
-void delayWithMsg(unsigned long duration, String message1, String message2, int statusCode) {
+void delayWithMsg(unsigned long duration, String message1, String message2, int statusCode)
+{
     unsigned long startTime = millis();
     lcd.clear();
     lcd.print(message1);
     lcd.setCursor(0, 1);
     lcd.print(message2);
-    while (millis() - startTime < duration) {
+    while (millis() - startTime < duration)
+    {
         ledStatusCode(statusCode);
     }
 }
 
-int readCapacitiveSensorData() {
+int readCapacitiveSensorData()
+{
     delay(100);
     return digitalRead(capacitiveSensorPin);
 }
 
-int readInductiveSensorData() {
+int readInductiveSensorData()
+{
     delay(100);
     return digitalRead(inductiveSensorPin);
 }
 
-
-void rotateServo(Servo &servo, int angle) {
+void rotateServo(Servo &servo, int angle)
+{
     servo.write(angle);
 }
 
-void openCloseBinLid(int lidNum, bool toOpen) {
+void openCloseBinLid(int lidNum, bool toOpen)
+{
     int angle = toOpen ? 180 : 0;
-    if (lidNum == 1) {
+    if (lidNum == 1)
+    {
         rotateServo(servo1, angle);
-    } else if (lidNum == 2) {
+    }
+    else if (lidNum == 2)
+    {
         rotateServo(servo2, angle);
     }
 }
 
-void waitToRemoveObject() {
+void waitToRemoveObject()
+{
     lcd.clear();
     lcd.print("Remove object....");
     openCloseBinLid(1, true);
-    while (readCapacitiveSensorData() == 1 || readInductiveSensorData() == 0) {
+    while (readCapacitiveSensorData() == 1 || readInductiveSensorData() == 0)
+    {
         ledStatusCode(404);
     }
     isObjectInside = false;
@@ -156,11 +176,14 @@ void waitToRemoveObject() {
     ledStatusCode(200);
 }
 
-void waitForObjectPresence() {
+void waitForObjectPresence()
+{
     unsigned long startTime = millis();
-    while (readCapacitiveSensorData() == 0) {
+    while (readCapacitiveSensorData() == 0)
+    {
         ledStatusCode(102);
-        if (millis() - startTime >= 3000) {
+        if (millis() - startTime >= 3000)
+        {
             ledStatusCode(404);
             delayWithMsg(2000, "No object", "detected!", 404);
             delayWithMsg(2000, "Lid closing", "remove hand", 404);
@@ -174,18 +197,23 @@ void waitForObjectPresence() {
     isObjectInside = true;
 }
 
-bool verifyObject() {
-    if (isObjectInside) {
+bool verifyObject()
+{
+    if (isObjectInside)
+    {
         delayWithMsg(3000, "Lid is closing...", "Remove hand!!!", 404);
         openCloseBinLid(1, false);
 
         unsigned long startTime = millis();
-        while (readCapacitiveSensorData() == 1) {
+        while (readCapacitiveSensorData() == 1)
+        {
             ledStatusCode(102);
             lcd.clear();
             lcd.print("Verifying....");
-            if (millis() - startTime >= 2000) {
-                if (readCapacitiveSensorData() == 1 && readInductiveSensorData() == 1) {
+            if (millis() - startTime >= 2000)
+            {
+                if (readCapacitiveSensorData() == 1 && readInductiveSensorData() == 1)
+                {
                     lcd.clear();
                     lcd.print("Verified!");
                     ledStatusCode(200);
@@ -194,7 +222,9 @@ bool verifyObject() {
                     openCloseBinLid(2, false);
                     delay(3000);
                     return true;
-                } else {
+                }
+                else
+                {
                     lcd.clear();
                     lcd.print("Invalid");
                     waitToRemoveObject();
@@ -208,7 +238,8 @@ bool verifyObject() {
     return false;
 }
 
-void updateMenuDisplay() {
+void updateMenuDisplay()
+{
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(currentMenu->title);
@@ -217,49 +248,111 @@ void updateMenuDisplay() {
     lcd.print(currentMenu->items[currentMenu->currentItem].name);
 }
 
-void navigateMenu(int direction) {
+void navigateMenu(int direction)
+{
     currentMenu->currentItem += direction;
-    if (currentMenu->currentItem < 0) currentMenu->currentItem = currentMenu->itemCount - 1;
-    if (currentMenu->currentItem >= currentMenu->itemCount) currentMenu->currentItem = 0;
+    if (currentMenu->currentItem < 0)
+        currentMenu->currentItem = currentMenu->itemCount - 1;
+    if (currentMenu->currentItem >= currentMenu->itemCount)
+        currentMenu->currentItem = 0;
     updateMenuDisplay();
     delay(200);
-    while (digitalRead(direction > 0 ? downButton : upButton) == LOW);
+    while (digitalRead(direction > 0 ? downButton : upButton) == LOW)
+        ;
 }
 
-void selectMenuItem() {
+void selectMenuItem()
+{
     currentMenu->items[currentMenu->currentItem].action();
     updateMenuDisplay();
     delay(200);
-    while (digitalRead(selectButton) == LOW);
+    while (digitalRead(selectButton) == LOW)
+        ;
 }
 
-void depositAction() {
+void depositAction()
+{
     lcd.clear();
     lcd.print("Opening bin....");
     openCloseBinLid(1, true);
     lcd.clear();
     lcd.print("Insert Bottle!");
     waitForObjectPresence();
-    if (verifyObject()) {
-        totalPoints += 1;  // Add points for successful deposit
+    if (verifyObject())
+    {
+        totalPoints += 1; // Add points for successful deposit
         lcd.clear();
         lcd.print("Deposit success!");
         lcd.setCursor(0, 1);
         lcd.print("Points: ");
         lcd.print(totalPoints);
         delay(2000);
+
+        // Post Deposit Menu
+        currentMenu = &postDepositMenu;
+        updateMenuDisplay();
     }
 }
-void redeemAction() {
+void insertAnotherBottleAction()
+{
+    depositAction();
+}
+void storePointsAction()
+{
+    lcd.clear();
+    lcd.print("Present RFID Card");
+    unsigned long startTime = millis();
+    while (millis() - startTime < 10000)
+    { // Wait for 10 seconds
+        if (detectCard())
+        {
+            if (writePoints(totalPoints))
+            {
+                lcd.clear();
+                lcd.print("Points stored!");
+                lcd.setCursor(0, 1);
+                lcd.print("Total: ");
+                lcd.print(totalPoints);
+                ledStatusCode(200);
+            }
+            else
+            {
+                lcd.clear();
+                lcd.print("Failed to store");
+                lcd.setCursor(0, 1);
+                lcd.print("points. Try again.");
+                ledStatusCode(404);
+            }
+            delay(2000);
+            mfrc522.PICC_HaltA();
+            mfrc522.PCD_StopCrypto1();
+            currentMenu = &mainMenu; // Return to main menu
+            updateMenuDisplay();
+            totalPoints = 0;
+            return;
+        }
+        delay(100);
+    }
+    lcd.clear();
+    lcd.print("No card detected");
+    delay(2000);
+    currentMenu = &mainMenu; // Return to main menu
+    updateMenuDisplay();
+}
+void redeemAction()
+{
     lcd.clear();
     lcd.print("Present RFID card");
-    
+
     unsigned long startTime = millis();
-    while (millis() - startTime < 10000) { // Wait for 10 seconds
-        if (detectCard()) {
+    while (millis() - startTime < 10000)
+    { // Wait for 10 seconds
+        if (detectCard())
+        {
             int currentPoints = readPoints();
-            if (currentPoints >= 0) {
-                totalPoints = currentPoints;  // Update totalPoints from RFID
+            if (currentPoints >= 0)
+            {
+                totalPoints = currentPoints; // Update totalPoints from RFID
                 pointsToRedeem = 0;
                 redeemPointsAction();
                 mfrc522.PICC_HaltA();
@@ -283,7 +376,7 @@ void redeemPointsAction() {
 
     unsigned long lastButtonPress = 0;
     int holdTime = 0;
-    const int maxRedeemable = min(totalPoints, 50);  // Max 50 points or total points, whichever is less
+    const int maxRedeemable = min(totalPoints, 20);  // Max 50 points or total points, whichever is less
     delay(1000);
     while (true) {
         lcd.setCursor(8, 1);
@@ -332,6 +425,8 @@ void redeemPointsAction() {
                 ledStatusCode(404);
             }
             delay(2000);
+            currentMenu = &mainMenu;  // Return to main menu
+            updateMenuDisplay();
             break;
         }
 
@@ -343,13 +438,16 @@ void redeemPointsAction() {
             lcd.print("Cancelled");
             delay(2000);
             pointsToRedeem = 0;
+            currentMenu = &mainMenu;  // Return to main menu
+            updateMenuDisplay();
             break;
         }
 
         delay(50);  // Small delay for button debounce
     }
 }
-void setup() {
+void setup()
+{
     lcd.init();
     lcd.backlight();
     lcd.clear();
@@ -361,62 +459,73 @@ void setup() {
     pinMode(PIN_RED, OUTPUT);
     pinMode(PIN_GREEN, OUTPUT);
     pinMode(PIN_BLUE, OUTPUT);
-    
+
     ledStatusCode(200);
 
     servo1.attach(servoPin1);
     servo2.attach(servoPin2);
     openCloseBinLid(1, false);
     openCloseBinLid(2, false);
-    
+
     updateMenuDisplay();
     Serial.begin(9600);
     setUpRFID();
 }
 
-void loop() {
-    if (digitalRead(downButton) == LOW) {
+void loop()
+{
+    if (digitalRead(downButton) == LOW)
+    {
         navigateMenu(1);
     }
-    if (digitalRead(upButton) == LOW) {
+    if (digitalRead(upButton) == LOW)
+    {
         navigateMenu(-1);
     }
-    if (digitalRead(selectButton) == LOW) {
+    if (digitalRead(selectButton) == LOW)
+    {
         selectMenuItem();
     }
 }
 
 // RFID Functions
 
-void setUpRFID(){
+void setUpRFID()
+{
     SPI.begin();
     mfrc522.PCD_Init();
-    for(byte i = 0; i < 6; i++){
+    for (byte i = 0; i < 6; i++)
+    {
         key.keyByte[i] = 0xFF;
     }
     Serial.println("RFID System ready. Present a card.");
 }
 
-bool detectCard(){
-    if(mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()){
+bool detectCard()
+{
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+    {
         Serial.println("Card detected");
         return true;
     }
     return false;
 }
 
-int readPoints(){
+int readPoints()
+{
     byte buffer[18];
     byte size = sizeof(buffer);
 
     MFRC522::StatusCode status = authenticateBlock(POINTS_BLOCK);
-    if(status !=MFRC522::STATUS_OK){
+    if (status != MFRC522::STATUS_OK)
+    {
         Serial.println("Authentication Failed.");
         return -1;
     }
 
     status = mfrc522.MIFARE_Read(POINTS_BLOCK, buffer, &size);
-    if(status != MFRC522::STATUS_OK){
+    if (status != MFRC522::STATUS_OK)
+    {
         Serial.println("Reading failed.");
         return -1;
     }
@@ -424,19 +533,22 @@ int readPoints(){
     return points;
 }
 
-bool writePoints(int points) {
+bool writePoints(int points)
+{
     byte buffer[16];
     buffer[0] = (points >> 8) & 0xFF; // High byte
     buffer[1] = points & 0xFF;        // Low byte
 
     MFRC522::StatusCode status = authenticateBlock(POINTS_BLOCK);
-    if (status != MFRC522::STATUS_OK) {
+    if (status != MFRC522::STATUS_OK)
+    {
         Serial.println("Authentication failed");
         return false;
     }
 
     status = mfrc522.MIFARE_Write(POINTS_BLOCK, buffer, 16);
-    if (status != MFRC522::STATUS_OK) {
+    if (status != MFRC522::STATUS_OK)
+    {
         Serial.println("Writing failed");
         return false;
     }
@@ -444,28 +556,33 @@ bool writePoints(int points) {
     return true;
 }
 
-void incrementPoints(int amount){
+void incrementPoints(int amount)
+{
     int currentPoints = readPoints();
-    if(currentPoints < 0) return;
+    if (currentPoints < 0)
+        return;
 
     int newPoints = currentPoints + amount;
-    if(writePoints(newPoints))
+    if (writePoints(newPoints))
         displayPoints(newPoints);
-    
 }
 
-void decrementPoints(int amount){
+void decrementPoints(int amount)
+{
     int currentPoints = readPoints();
-    if(currentPoints < 0) return;
+    if (currentPoints < 0)
+        return;
 
     int newPoints = currentPoints - amount;
-    if(newPoints < 0) newPoints = 0; //Prevents point from dropping to negative 
+    if (newPoints < 0)
+        newPoints = 0; // Prevents point from dropping to negative
 
-    if(writePoints(newPoints)) 
+    if (writePoints(newPoints))
         displayPoints(newPoints);
 }
 
-MFRC522::StatusCode authenticateBlock(int blockNumber){
+MFRC522::StatusCode authenticateBlock(int blockNumber)
+{
     return mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,
-    blockNumber, &key, &(mfrc522.uid));
+                                    blockNumber, &key, &(mfrc522.uid));
 }
